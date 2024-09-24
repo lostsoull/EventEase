9bcfb6248796aa184f97a4a6aa3ca380e8c1e121
lostsoull <yassinealo@yahoo.com>
2024-09-24 19:23:41 +0100

    LasUpdate3

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -65,7 +65,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
       try {
         console.log("TEST3")
         const newEvent = await createEvent({
-          event: { ...values, imageUrl: "its working" },
+          event: { ...values, imageUrl:  },
           userId,
           path: '/profile'
         })
