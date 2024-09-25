669052b33af433b3f4efb39979e5d3646aea87e0
lostsoull <yassinealo@yahoo.com>
2024-09-25 02:15:21 +0100

    adding static images to events

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -64,7 +64,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
     if (type === 'Create') {
       try {
         const newEvent = await createEvent({
-          event: { ...values, imageUrl: uploadedImageUrl },
+          event: { ...values, imageUrl: "https://images.pexels.com/photos/716281/pexels-photo-716281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
           userId,
           path: '/profile'
         })
